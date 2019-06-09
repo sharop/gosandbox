90de6a01696a7b6def381e5b5e9add5b8a800efa
sharop <sharop@gmail.com>
2019-06-08 22:13:19 -0500

    Patterns sandbox added

---
+++ Patterns/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"go.uber.org/fx"
+)
+
+// NewLogger Creacion de logger
+// Esta funcion no tiene relacion con Fx
+func NewLogger() *log.Logger {
+	logger := log.New(os.Stdout, "" /* prefix*/, 0 /* flags */)
+	logger.Print("Executing NewLogger.")
+	return logger
+}
+
+// NewHandler construye un manejador HTTP simple.
+// Fx tratara NeHandler como el constructor para el tipo http.Handler
+func NewHandler(logger *log.Logger) (http.Handler, error) {
+	logger.Print("Executing NewHandler.")
+	return http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		logger.Print("Got a request")
+	}), nil
+}
+
+//NewMux constructs an HTTP mux.
+func NewMux(lc fx.Lifecycle, logger *log.Logger) *http.ServeMux {
+	logger.Print("Executing NewMux.")
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			logger.Print("Starting HTTP Server.")
+			go server.ListenAndServe()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Print("Stopping HTTP server.")
+			return server.Shutdown(ctx)
+		},
+	})
+
+	return mux
+}
+func Register(mux *http.ServeMux, h http.Handler) {
+	mux.Handle("/", h)
+}
+
+func main() {
+	app := fx.New(
+		// Provide all the constructors we need, which teaches Fx how we'd like to
+		// construct the *log.Logger, http.Handler, and *http.ServeMux types.
+		// Remember that constructors are called lazily, so this block doesn't do
+		// much on its own.
+		fx.Provide(
+			NewLogger,
+			NewHandler,
+			NewMux,
+		),
+		// Since constructors are called lazily, we need some invocations to
+		// kick-start our application. In this case, we'll use Register. Since it
+		// depends on an http.Handler and *http.ServeMux, calling it requires Fx
+		// to build those types using the constructors above. Since we call
+		// NewMux, we also register Lifecycle hooks to start and stop an HTTP
+		// server.
+		fx.Invoke(Register),
+	)
+
+	// In a typical application, we could just use app.Run() here. Since we
+	// don't want this example to run forever, we'll use the more-explicit Start
+	// and Stop.
+	startCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Start(startCtx); err != nil {
+		log.Fatal(err)
+	}
+
+	// Normally, we'd block here with <-app.Done(). Instead, we'll make an HTTP
+	// request to demonstrate that our server is running.
+	http.Get("http://localhost:8080/")
+
+	stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Stop(stopCtx); err != nil {
+		log.Fatal(err)
+	}
+}
